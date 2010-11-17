e4ab89929ca7fea9455ad01e1008fa101d29ac5c
SSozonoff <SSozonoff@595dc79c-e44b-dc71-6522-08eb688d7ee5>
2010-11-17 23:41:42 +0000

    Charge status stuff is still screwed up

--- SNAPpy/rf_charger.py
+++ SNAPpy/rf_charger.py
@@ -4,28 +4,95 @@ from ds2764_ss import *
 
 portalAddr = '\x00\x00\x01' # hard-coded address for Portal <------------<<<<<<<<
 
+CHG_ERROR = GPIO_9
+CHG_STATUS = GPIO_10
+
+isChargingPreviousState = False
+chgStatusBlinkTimer = 0
+chgStatusBlinkCounter = 0
+
+""" 
+valid options for this are 
+N - Not charging 
+C - Charging 
+D - Done Charging
+- - unknown
+"""
+chargeStatus = '-'
+
+noChgError = False
+
 @setHook(HOOK_STARTUP)
-def start():
+def start():    
+    global noChgError, chgStatusBlinkTimer, isChargingPreviousState
+    setPinDir(CHG_ERROR, False)
+    setPinDir(CHG_STATUS, False)
+    monitorPin(CHG_ERROR, True)
+    monitorPin(CHG_STATUS, True)
+    
+    noChgError = readPin(CHG_ERROR)
+    isChargingPreviousState = readPin(CHG_STATUS)
+    
     # Go ahead and redirect STDOUT to Portal now
     ucastSerial("\x00\x00\x01") # put your correct Portal address here!
     crossConnect(DS_STDIO,DS_TRANSPARENT)
         
     # I2C GPIO_17/18 used for MCP3424
-    print "calling i2cInit"
     i2cInit(True)
     
+    chgStatusBlinkTimer = getMs()
+    
+    
+@setHook(HOOK_GPIN)
+def pinHandler(pin, isSet):
+    global noChgError, chgStatusBlinkTimer, chgStatusBlinkCounter, isChargingPreviousState, chargeStatus
+    if (pin == CHG_ERROR):
+        noChgError = isSet
+    if (pin == CHG_STATUS):
+        chgStatusBlinkTimer = getMs()        
+        if (isSet != isChargingPreviousState):
+            chgStatusBlinkCounter += 1
+        
+        if (chgStatusBlinkCounter >=3):
+            # charging complete
+            chargeStatus = 'D'
+            
+            # Roll the value before we overflow
+            if (chgStatusBlinkCounter >= 32767):
+                chgStatusBlinkCounter = 3
+                        
+        isChargingPreviousState = isSet        
+        
+                
 @setHook(HOOK_100MS)
 def triggeredAt100ms():
+    global chargeStatus
     DS2764FetchBasic()
+    
+    # manage the charge status indicator timer    
+    t = getMs()
+    if (t > chgStatusBlinkTimer + 2500):      
+        chargeStatus = 'N' if (readPin(CHG_STATUS)) else 'C'
+    
+def getChargeError():
+    return True if (not noChgError) else False
+
+def getChargeStatus():
+    return chargeStatus
     
 @setHook(HOOK_1S)
 def trigger():
+    cur = getDSCurrent()
+    if (cur < 0):
+        cur *= -1
     rpc(portalAddr, "logData", "Lipo voltage mV", getDSVoltage(), 4500)
-    rpc(portalAddr, "logData", "Current draw mA", getDSCurrent(), 100)    
+    rpc(portalAddr, "logData", "Current draw mA", cur, 100)    
     rpc(portalAddr, "logData", "Bat remain power", getDSACurrent(), 1000)
     
-    print "Lipo voltage --> " + str(getDSVoltage()) + "mV"
-    print "Lipo current --> " + str(getDSCurrent()) + "mA"
-    print "Lipo accumulated current --> " + str(getDSACurrent()) + "mAh"
+    #print "Lipo mV -> " + str(getDSVoltage()) + "mV"
+    #print "Lipo mA -> " + str(getDSCurrent()) + "mA"
+    #print "Lipo mAh -> " + str(getDSACurrent()) + "mAh"    
+    #print "Charging -> " + str(readPin(CHG_STATUS))
+    #print "Charging error " + str(chg_error)
     
     
